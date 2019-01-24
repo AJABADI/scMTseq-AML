17ecfcc2285bb959b83777e19b7d5ab761132775
Al <ajalalabadi@unimelb.edu.au>
2019-01-24 18:25:13 +1100

    plot.pca to be able to differentiate matching cells from others

--- Rproj_scMTseq_AML/utils/plot.pca.R
+++ Rproj_scMTseq_AML/utils/plot.pca.R
@@ -1,21 +1,54 @@
 #################### functions to plot pca objects using ggplot2
 ## INPUTS: 
-      ## pca.obj: a mixOmics' pca object with at least 3 components
-      ## top: a title
+## pca.obj: a mixOmics' pca object with at least 3 components
+## top: a title
 ## OUTPUT:
-      ## a grid plot of explained variance in each pcs and pcs against each other
+## a grid plot of explained variance in each pcs and pcs against each other
+library(gridExtra)
+library(ggplot2)
 
-gg.pca = function(pca=pca.scran, PCs = c(1,2), col='darkblue'){
-  mat = as.data.frame(pca$variates$X)
-  p = ggplot(mat) + geom_point(aes(x=mat[,PCs[1]], y=mat[,PCs[2]]), col =col)  +
-    labs(x = paste0('PC',PCs[1]), y= paste0('PC',PCs[2]))+
-    theme_bw() + 
-    theme(plot.title = element_text(hjust = 0.5))
-  return(p)
-}
+# pca.obj=pca.res
+# col='darkblue'
+# diff.samples = NULL
+# default.label = "Matching"
+# diff.label = "RNA-seq only"
+# point.label=F
+
+
+# pca = pca.obj
+# PCs = c(1,2)
 
 ## multi pca + variance plot
-plot.pca = function(gg.pca=gg.pca, pca.obj=pca.scran, top = 'scran-normalised'){
+pca.grid = function(pca.obj=pca.res, col='darkblue', diff.samples = NULL,
+                    default.label = "Matching", diff.label = "RNA-seq only", point.label=F){
+  
+  gg.pca = function(pca=pca.res, PCs = c(1,2), label = point.label ){
+    mat = as.data.frame(pca$variates$X)
+    if(is.null(diff.samples)){
+      p = ggplot(mat) +
+        labs(x = paste0('PC',PCs[1]), y= paste0('PC',PCs[2]))+
+        theme_bw() + 
+        theme(plot.title = element_text(hjust = 0.5))
+      
+      if(label){
+        p = p + geom_point(aes(x=mat[,PCs[1]], y=mat[,PCs[2]],
+                               label=rownames(mat)), col =col)+ geom_text(aes(label=rownames(mat)))
+      } else {
+        p = p + geom_point(aes(x=mat[,PCs[1]], y=mat[,PCs[2]]), col =col)
+      }
+    } else {
+      mat$colors = default.label
+      mat[diff.samples,]$colors = diff.label
+      p = ggplot(mat) + geom_point(aes(x=mat[,PCs[1]], y=mat[,PCs[2]], col = colors))  +
+        labs(x = paste0('PC',PCs[1]), y= paste0('PC',PCs[2]))+
+        theme_bw() + 
+        theme(plot.title = element_text(hjust = 0.5)) + 
+        guides(col=guide_legend(title="cells"))
+    }
+    return(p)
+  }
+  
+  
   exp.var = data.frame(pca.obj$explained_variance)
   p1 = ggplot(exp.var) +geom_col(aes(x=rownames(exp.var), y=exp.var[,1]))+
     labs(x='PCs', y= 'Explained Variance') + ggtitle('Explained Variance') +
@@ -24,6 +57,6 @@ plot.pca = function(gg.pca=gg.pca, pca.obj=pca.scran, top = 'scran-normalised'){
   p2 = gg.pca(pca=pca.obj, PCs = c(1,2))
   p3 = gg.pca(pca=pca.obj, PCs = c(2,3))
   p4 = gg.pca(pca=pca.obj, PCs = c(1,3))
-  grid = grid.arrange(p1,p2,p3,p4, top= textGrob(top, gp=gpar(col="darkorange", fontsize=18 )))
+  grid = grid.arrange(p1,p2,p3,p4)
   return(grid)
 }
