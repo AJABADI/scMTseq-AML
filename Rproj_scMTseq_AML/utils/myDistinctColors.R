fd6497086042101fedc68b2aa29a9f2360d978f2
Al <ajalalabadi@unimelb.edu.au>
2019-01-24 18:24:16 +1100

    Added a function to create distinct colors for visualisation

---
+++ Rproj_scMTseq_AML/utils/myDistinctColors.R
@@ -0,0 +1,19 @@
+## 14 distinct colors
+
+myDistinctColors = c("#000000", ## black
+                     "#3cb44b", ## green
+                     "#000075", ## marine blue
+                     "#42d4f4", ## cyan
+                     "#f58231", ## orange
+                     "#e6194B", ## red but pink
+                     "#ffe119", ## yellow
+                     "#911eb4", ## purple
+                     "#9A6324", ## brown
+                     "#808000", ## olive
+                     "#f032e6", ## magneta (purple-pink)
+                     "#800000", ## maroon
+                     "#a9a9a9", ## grey
+                     "#bfef45"  ## lime
+                   
+)
+
