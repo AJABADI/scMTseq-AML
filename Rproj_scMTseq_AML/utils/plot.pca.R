cc73970b38b54139b479c5c19fad787b0f8e7fc1
Al <ajalalabadi@unimelb.edu.au>
2019-02-06 18:30:02 +1100

    included the main title

--- Rproj_scMTseq_AML/utils/plot.pca.R
+++ Rproj_scMTseq_AML/utils/plot.pca.R
@@ -20,7 +20,7 @@ library(ggplot2)
 
 ## multi pca + variance plot
 pca.grid = function(pca.obj=pca.res, col='darkblue', diff.samples = NULL,
-                    default.label = "Matching", diff.label = "RNA-seq only", point.label=F){
+                    default.label = "Matching", diff.label = "RNA-seq only", point.label=F, top = ""){
   
   gg.pca = function(pca=pca.res, PCs = c(1,2), label = point.label ){
     mat = as.data.frame(pca$variates$X)
@@ -31,8 +31,11 @@ pca.grid = function(pca.obj=pca.res, col='darkblue', diff.samples = NULL,
         theme(plot.title = element_text(hjust = 0.5))
       
       if(label){
-        p = p + geom_point(aes(x=mat[,PCs[1]], y=mat[,PCs[2]],
-                               label=rownames(mat)), col =col)+ geom_text(aes(label=rownames(mat)))
+        well = substr(rownames(mat),0,1)
+        p = p + 
+          geom_text(aes(x=mat[,PCs[1]], y=mat[,PCs[2]],label=rownames(mat),
+                        col = well),hjust=0, vjust=0) +
+          scale_color_manual(values = color.mixo(1:length(well)))
       } else {
         p = p + geom_point(aes(x=mat[,PCs[1]], y=mat[,PCs[2]]), col =col)
       }
@@ -57,6 +60,6 @@ pca.grid = function(pca.obj=pca.res, col='darkblue', diff.samples = NULL,
   p2 = gg.pca(pca=pca.obj, PCs = c(1,2))
   p3 = gg.pca(pca=pca.obj, PCs = c(2,3))
   p4 = gg.pca(pca=pca.obj, PCs = c(1,3))
-  grid = grid.arrange(p1,p2,p3,p4)
+  grid = grid.arrange(p1,p2,p3,p4, top =textGrob(top,gp=gpar(fontsize=16,font=3, col = "blue")))
   return(grid)
 }
