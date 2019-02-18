a85a529cde905282089ae3923f2b91a99b69f0d0
Al <ajalalabadi@unimelb.edu.au>
2019-02-18 18:12:45 +1100

    a vector of custom colors

--- Rproj_scMTseq_AML/utils/myDistinctColors.R
+++ Rproj_scMTseq_AML/utils/myDistinctColors.R
@@ -1,19 +1,17 @@
 ## 14 distinct colors
 
-myDistinctColors = c("#000000", ## black
-                     "#3cb44b", ## green
+myDistinctColors = c("#3cb44b", ## green
                      "#000075", ## marine blue
-                     "#42d4f4", ## cyan
                      "#f58231", ## orange
                      "#e6194B", ## red but pink
-                     "#ffe119", ## yellow
                      "#911eb4", ## purple
-                     "#9A6324", ## brown
-                     "#808000", ## olive
                      "#f032e6", ## magneta (purple-pink)
-                     "#800000", ## maroon
+                     "#808000", ## olive
                      "#a9a9a9", ## grey
+                     "#42d4f4", ## cyan
+                     "#800000", ## maroon
+                     "#ffe119", ## yellow
+                     "#9A6324", ## brown
                      "#bfef45"  ## lime
-                   
-)
+                     )
 
